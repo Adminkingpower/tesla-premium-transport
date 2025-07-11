3d52999aabf2fb72e8cc7f1d1c546826b40c8f81
adminkingpower <info@kingpowerau.com>
2025-07-12 03:00:41 +1000

    Auto-create table on first request (Render compatible)

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from kp import create_app
+from kp import create_app, db
 
 app = create_app()
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
